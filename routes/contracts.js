3b68c3308268f11f7993610f0f665e8be537c227
Israel Camargo <isfelipecm@gmail.com>
2022-02-04 19:06:52 -0500

    Permissions changed

--- routes/contracts.js
+++ routes/contracts.js
@@ -22,31 +22,16 @@ router.get("/materials/edit/:id", checkAuth, contractsController.getMaterial);
 router.get("/tools/edit/:id", checkAuth, contractsController.getTool);
 
 //POST
-router.post(
-	"/newcontract",
-	checkAuth,
-	checkRoleSU,
-	contractsController.createContract
-);
+router.post("/newcontract", checkAuth, contractsController.createContract);
 
-router.post(
-	"/newmaterial",
-	checkAuth,
-	checkRoleSU,
-	contractsController.createMaterial
-);
+router.post("/newmaterial", checkAuth, contractsController.createMaterial);
 
-router.post("/addtool", checkAuth, checkRoleSU, contractsController.createTool);
+router.post("/addtool", checkAuth, contractsController.createTool);
 
 //UPDATE
-router.put("/:id", checkAuth, checkRoleSU, contractsController.updateContract);
-router.put(
-	"/material/:id",
-	checkAuth,
-	checkRoleSU,
-	contractsController.updateMaterial
-);
-router.put("/tool/:id", checkAuth, checkRoleSU, contractsController.updateTool);
+router.put("/:id", checkAuth, contractsController.updateContract);
+router.put("/material/:id", checkAuth, contractsController.updateMaterial);
+router.put("/tool/:id", checkAuth, contractsController.updateTool);
 
 //ELIMINATE
 router.delete(
